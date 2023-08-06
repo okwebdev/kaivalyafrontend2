7610be46cd8bcef0b403da6fd1c4548cb3af516d
okwebdev <okwebdev9@gmail.com>
2023-08-06 14:01:00 +0100

    fixing logo img src

--- src/components/MainLogo.vue
+++ src/components/MainLogo.vue
@@ -1,22 +1,25 @@
 <template>
     <div @click="$emit('toggle')" class="" id="Logo-container">
   
-        <div id="logo">
+        <!-- <div id="logo">
           <img :src="image"/>
-        </div>
+        </div> -->
         
+        <div class="mainlogo">
+          <img src="../assets/img/kaivalya-name-min.png" alt="">
+        </div>
     </div>
   </template>
     
   <script>
-    import image from "/Users/oscarkhan/repos/kaivalya_website/kaivalya_vue/src/assets/img/kaivalya-name-min.png"
+    // import image from "/Users/oscarkhan/repos/kaivalya_website/kaivalya_vue/src/assets/img/kaivalya-name-min.png"
     
     export default {
-        data: function () {
-            return {
-                image: image
-            }
-        }
+        // data: function () {
+        //     return {
+        //         image: image
+        //     }
+        // }
     }
   </script>
     
